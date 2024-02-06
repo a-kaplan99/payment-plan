293cc85ae833d6882afbba1f34fbd4a6b5d418ab
kaplan <kaplan@lucasware.com>
2024-02-05 23:13:44 -0500

    Implement some basic functions

--- SoftwareEngineerInterview/CSharp/Zip.InstallmentsService/Zip.InstallmentsService.Test/PaymentPlanFactoryTests.cs
+++ SoftwareEngineerInterview/CSharp/Zip.InstallmentsService/Zip.InstallmentsService.Test/PaymentPlanFactoryTests.cs
@@ -1,4 +1,6 @@
 using Shouldly;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace Zip.InstallmentsService.Test
@@ -12,10 +14,52 @@ namespace Zip.InstallmentsService.Test
             var paymentPlanFactory = new PaymentPlanFactory();
             
             // Act
-            var paymentPlan = paymentPlanFactory.CreatePaymentPlan(123.45M);
+            var paymentPlan = PaymentPlanFactory.CreatePaymentPlan(123.45M);
 
             // Assert
             paymentPlan.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void WhenCreatePaymentPlanWithInvalidOrderAmount_ShouldReturnNull()
+        {
+            // Arrange
+            var paymentPlanFactory = new PaymentPlanFactory();
+
+            // Act
+            var paymentPlan1 = PaymentPlanFactory.CreatePaymentPlan(-123.45M);
+            var paymentPlan2 = PaymentPlanFactory.CreatePaymentPlan(0);
+
+            // Assert
+            paymentPlan1.ShouldBeNull();
+            paymentPlan2.ShouldBeNull();
+        }
+
+        [Fact]
+        public void WhenCreatePaymentPlanWithValidOrderAmount_ShouldReturnPaymentPlanWithDEfaultPurchaseAmountAndInstallments()
+        {
+            // Arrange
+            var paymentPlanFactory = new PaymentPlanFactory();
+            var expectedAmount = 123.45M;
+            var expecedNumInstallments = 4;
+            var expectedFrequency = 14;
+
+            // Act
+            var paymentPlan = PaymentPlanFactory.CreatePaymentPlan(expectedAmount);
+
+            // Assert
+            paymentPlan.ShouldNotBeNull();
+            paymentPlan.PurchaseAmount.ShouldBe(expectedAmount);
+            paymentPlan.Installments.Length.ShouldBe(expecedNumInstallments);
+
+            // Assert Installments
+            var now = DateTime.Today;
+            for (var i = 1; i <= expecedNumInstallments; i++)
+            {
+                var installment = paymentPlan.Installments[i - 1];
+                installment.Amount.ShouldBe(expectedAmount / expecedNumInstallments);
+                installment.DueDate.ShouldBe(now.AddDays(expectedFrequency * i));
+            }
         }
     }
 }
--- SoftwareEngineerInterview/CSharp/Zip.InstallmentsService/Zip.InstallmentsService/PaymentPlanFactory.cs
+++ SoftwareEngineerInterview/CSharp/Zip.InstallmentsService/Zip.InstallmentsService/PaymentPlanFactory.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Zip.InstallmentsService
 {
     /// <summary>
@@ -5,15 +7,53 @@ namespace Zip.InstallmentsService
     /// </summary>
     public class PaymentPlanFactory
     {
+        // Default Settings for payment plan
+        const int FREQUENCY = 14;
+        const int NUM_INSTALLMENTS = 4;
+
         /// <summary>
         /// Builds the PaymentPlan instance.
         /// </summary>
         /// <param name="purchaseAmount">The total amount for the purchase that the customer is making.</param>
         /// <returns>The PaymentPlan created with all properties set.</returns>
-        public PaymentPlan CreatePaymentPlan(decimal purchaseAmount)
+        public static PaymentPlan CreatePaymentPlan(decimal purchaseAmount)
         {
-            // TODO
-            return new PaymentPlan();
+            // Error scenario
+            if (purchaseAmount <= 0)
+            {
+                Console.WriteLine("Cannot compute payment plan for amount less than or equal to 0.");
+                return null;
+            }
+
+            return new PaymentPlan
+            {
+                Id = Guid.NewGuid(),
+                PurchaseAmount = purchaseAmount,
+                Installments = GenerateInstallments(purchaseAmount, NUM_INSTALLMENTS, FREQUENCY)
+            }; ;
+        }
+
+        // Helper Methods
+
+        private static Installment[] GenerateInstallments(decimal purchaseAmount, int numInstallments, int frequency)
+        {
+            // One-time computed values
+            var installments = new Installment[numInstallments];
+            var startDate = DateTime.Today;
+            var amountPerInstallment = purchaseAmount / numInstallments;
+
+            // Compute and add installments
+            for (int i = 1; i <= numInstallments; i++)
+            {
+                installments[i - 1] = new Installment
+                {
+                    Id = Guid.NewGuid(),
+                    Amount = amountPerInstallment,
+                    DueDate = startDate.AddDays(frequency * i)
+                };
+            }
+
+            return installments;
         }
     }
 }
